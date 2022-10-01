f792b501905f23e28243ba575c37ef7986af06a1
typeerrordev <matthew.pantel86@gmail.com>
2022-09-30 20:59:54 -0500

    ADD: Added Matthew Pantel as a student, along with additional profile information

--- js/data.js
+++ js/data.js
@@ -1,25 +1,34 @@
 const data = [
-    {
-        name: "Rachel Johnson",
-        treehouse_username: "rachelktyjohnson",
-        techdegree_slug: "data",
-        favorite_course_name: "Copyright Basics",
-        favorite_course_url: "https://teamtreehouse.com/library/copyright-basics"
-    },
-    {
-        name: "Rohald van Merode",
-        treehouse_username: "rohald89",
-        techdegree_slug: "fsjs",
-        favorite_course_name: "How to Learn Online Like a Pro",
-        favorite_course_url: "https://teamtreehouse.com/library/how-to-learn-online-like-a-pro-with-guil-hernandez"
-    }
-]
+  {
+    name: "Rachel Johnson",
+    treehouse_username: "rachelktyjohnson",
+    techdegree_slug: "data",
+    favorite_course_name: "Copyright Basics",
+    favorite_course_url: "https://teamtreehouse.com/library/copyright-basics",
+  },
+  {
+    name: "Rohald van Merode",
+    treehouse_username: "rohald89",
+    techdegree_slug: "fsjs",
+    favorite_course_name: "How to Learn Online Like a Pro",
+    favorite_course_url:
+      "https://teamtreehouse.com/library/how-to-learn-online-like-a-pro-with-guil-hernandez",
+  },
+  {
+    name: "Matthew Pantel",
+    treehouse_username: "matthewpantel",
+    techdegree_slug: "fsjs",
+    favorite_course_name: "Modular CSS with Sass",
+    favorite_course_url:
+      "https://teamtreehouse.com/library/modular-css-with-sass",
+  },
+];
 
 const slug_map = {
-    "fewd": "Front End Web Development TechDegree",
-    "fsjs": "Full Stack Javascript TechDegree",
-    "web": "Web Development TechDegree",
-    "python": "Python TechDegree",
-    "data": "Data Analysis TechDegree",
-    "ux": "User Experience TechDegree",
-}
+  fewd: "Front End Web Development TechDegree",
+  fsjs: "Full Stack Javascript TechDegree",
+  web: "Web Development TechDegree",
+  python: "Python TechDegree",
+  data: "Data Analysis TechDegree",
+  ux: "User Experience TechDegree",
+};
