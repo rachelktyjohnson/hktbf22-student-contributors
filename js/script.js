b72c5881e6ccd1fabe90ac92011bd7b98ab4e3dc
typeerrordev <matthew.pantel86@gmail.com>
2022-10-01 16:00:47 -0500

    ADD: Added randomized order to the list of students, regardless of the number of students within the data.js file

--- js/script.js
+++ js/script.js
@@ -1,23 +1,26 @@
-const student_cards = document.querySelector(".student-cards")
+const student_cards = document.querySelector(".student-cards");
 
 let student_cards_html = "";
 
-data.forEach((student) => {
+// Loop through the student data and display each student's information in a card, but randomized order
+// Resource: https://dev.to/codebubb/how-to-shuffle-an-array-in-javascript-2ikj
+const randomizeStudentDisplay = data
+  .sort((a, b) => 0.5 - Math.random())
+  .forEach((student) => {
     student_cards_html +=
-        `<div class='student-card ${student.techdegree_slug}'>` +
-        `<a target="_blank" href='https://teamtreehouse.com/profiles/${student.treehouse_username}'>` +
-        `<h3 class='student-name'>${student.name}</h3>` +
-        "</a>" +
-        "<div class='techdegree'>" +
-        "<h4>Current TechDegree</h4>" +
-        `<p>${slug_map[student.techdegree_slug]}</p>` +
-        "</div>" +
-        "<div class='favourite'>" +
-        "<h4>Favourite Video/Course</h4>" +
-        `<p><a href='${student.favorite_course_url}'>${student.favorite_course_name}</a></p>` +
-        "</div>" +
-        "</div>"
+      `<div class='student-card ${student.techdegree_slug}'>` +
+      `<a target="_blank" href='https://teamtreehouse.com/profiles/${student.treehouse_username}'>` +
+      `<h3 class='student-name'>${student.name}</h3>` +
+      "</a>" +
+      "<div class='techdegree'>" +
+      "<h4>Current TechDegree</h4>" +
+      `<p>${slug_map[student.techdegree_slug]}</p>` +
+      "</div>" +
+      "<div class='favourite'>" +
+      "<h4>Favourite Video/Course</h4>" +
+      `<p><a href='${student.favorite_course_url}'>${student.favorite_course_name}</a></p>` +
+      "</div>" +
+      "</div>";
+  });
 
-})
-
-student_cards.innerHTML = student_cards_html
+student_cards.innerHTML = student_cards_html;
